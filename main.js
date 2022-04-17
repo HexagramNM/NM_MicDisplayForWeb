a30134dc66823518d4ae2f18fd171ecda2a3df94
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2022-04-17 13:15:56 +0900

    カメラ、マイク設定、アイコンの追加
    最初にカメラやマイクをデフォルトから変更できるように修正。アイコンを追加

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+
+var VirtualBB_cameraDeviceLists;
+var VirtualBB_selectedCameraDeviceId;
+var VirtualBB_micDeviceLists;
+var VirtualBB_selectedMicDeviceId;
+
+function VirtualBB_getSelectedDeviceId() {
+    var cameraSelectIndex = document.getElementById("cameraSelect").selectedIndex;
+    var micSelectIndex = document.getElementById("micSelect").selectedIndex;
+
+    if (cameraSelectIndex <= 0) {
+        VirtualBB_selectedCameraDeviceId = null;
+    }
+    else {
+        VirtualBB_selectedCameraDeviceId = VirtualBB_cameraDeviceLists[cameraSelectIndex - 1].deviceId;
+    }
+
+    if (micSelectIndex <= 0) {
+        VirtualBB_selectedMicDeviceId = null;
+    }
+    else {
+        VirtualBB_selectedMicDeviceId = VirtualBB_micDeviceLists[micSelectIndex - 1].deviceId;
+    }
+}
+
 async function VirtualBB_startProcess() {
-    removeEventListener("click", VirtualBB_startProcess);
     document.oncontextmenu = VirtualBB_toggleMirror;
     document.getElementById("easyInst").style.display = "none";
 
@@ -9,10 +33,14 @@ async function VirtualBB_startProcess() {
     try {
         //Webカメラとvideoタグとの関連づけ
         //https://qiita.com/chelcat3/items/02c77b55d080d770530a
+        VirtualBB_getSelectedDeviceId();
         mediaStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
+            audio: {
+                deviceId: VirtualBB_selectedMicDeviceId ? {exact: VirtualBB_selectedMicDeviceId}: undefined
+            },
             video: {
                 //virtualBackCanvasSizeはglobalVariables.jsからの変数
+                deviceId: VirtualBB_selectedCameraDeviceId ? {exact: VirtualBB_selectedCameraDeviceId}: undefined,
                 width: {ideal: virtualBackCanvasSize.width},
                 height: {ideal: virtualBackCanvasSize.height}
             }
@@ -23,6 +51,7 @@ async function VirtualBB_startProcess() {
     }
     catch (err) {
         //ユーザに拒否されたなど、カメラ、マイクを取得できなかった場合
+        document.getElementById("errorPermission").style.display = "";
         return;
     }
 
@@ -53,7 +82,58 @@ async function VirtualBB_startProcess() {
     SharedWindow_main();
 }
 
-function VirtualBB_main() {
+async function VirtualBB_createDeviceSelector() {
+    VirtualBB_cameraDeviceLists = (await navigator.mediaDevices.enumerateDevices())
+        .filter((device) => device.kind === 'videoinput')
+        .map((device) => {
+            return {
+                name: device.label,
+                deviceId: device.deviceId
+            };
+        });
+
+    for (var idx = 0; idx < VirtualBB_cameraDeviceLists.length; idx++) {
+        var optionElem = document.createElement("option");
+        optionElem.text = VirtualBB_cameraDeviceLists[idx].name;
+        optionElem.value = VirtualBB_cameraDeviceLists[idx].name;
+        document.getElementById("cameraSelect").appendChild(optionElem);
+    }
+
+    VirtualBB_micDeviceLists = (await navigator.mediaDevices.enumerateDevices())
+        .filter((device) => device.kind === 'audioinput')
+        .map((device) => {
+            return {
+                name: device.label,
+                deviceId: device.deviceId
+            };
+        });
+    for (var idx = 0; idx < VirtualBB_micDeviceLists.length; idx++) {
+        var optionElem = document.createElement("option");
+        optionElem.text = VirtualBB_micDeviceLists[idx].name;
+        optionElem.value = VirtualBB_micDeviceLists[idx].name;
+        document.getElementById("micSelect").appendChild(optionElem);
+    }
+}
+
+async function VirtualBB_main() {
     document.bgColor=backgroundColorCode;
-    addEventListener("click", VirtualBB_startProcess);
+
+    //許可ダイアログの発生（確認後、各トラックは停止させる）
+    try {
+        mediaStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+        mediaStream.getTracks().forEach(track => track.stop());
+    }
+    catch(err) {
+        //拒否された場合
+        document.getElementById("easyInst").style.display = "none";
+        document.getElementById("errorPermission").style.display = "";
+        return;
+    }
+
+    VirtualBB_createDeviceSelector();
+
+    document.getElementById("startButton").addEventListener("click", VirtualBB_startProcess);
 }
