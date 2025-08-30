ea083fe3ba9ca0f39deeb6079d9deadbc957bcf5
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2025-08-31 00:26:02 +0900

    backMaskFshaderにclampを追加

--- shaders/backMaskFshader.frag.js
+++ shaders/backMaskFshader.frag.js
@@ -11,7 +11,7 @@ varying vec2 vTextureCoord;
 void main(void) {
   vec4 smpColor = texture2D(texture, vTextureCoord);
   float grayScale = smpColor.r * 0.299 + smpColor.g * 0.587 + smpColor.b * 0.114;
-  float newGrayScale = texture2D(cdf, vec2(grayScale, 0.0)).r;
+  float newGrayScale = clamp(texture2D(cdf, vec2(grayScale, 0.0)).r, 0.0, 1.0);
 
   float u = -0.169 * smpColor.r - 0.331 * smpColor.g + 0.5 * smpColor.b;
   float v = 0.5 * smpColor.r - 0.419 * smpColor.g - 0.081 * smpColor.b;
@@ -26,9 +26,9 @@ void main(void) {
   float mixU = weight * targetU + (1.0 - weight) * u;
   float mixV = weight * targetV + (1.0 - weight) * v;
 
-  smpColor.r = newGrayScale + 1.402 * mixV;
-  smpColor.g = newGrayScale - 0.344 * mixU - 0.714 * mixV;
-  smpColor.b = newGrayScale + 1.772 * mixU;
+  smpColor.r = clamp(newGrayScale + 1.402 * mixV, 0.0, 1.0);
+  smpColor.g = clamp(newGrayScale - 0.344 * mixU - 0.714 * mixV, 0.0, 1.0);
+  smpColor.b = clamp(newGrayScale + 1.772 * mixU, 0.0, 1.0);
   smpColor.a = step(0.5, smpColor.a);
 
   float sinValue = sin((vTextureCoord.y + time * 0.01) * 50.0 * PI);
